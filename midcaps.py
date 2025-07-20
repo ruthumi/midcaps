99ba7fca1dbd2da8520d0753cd7a948356e1241e
Ruthumithra <121571438+ruthumi@users.noreply.github.com>
2025-07-20 15:11:53 +0400

    Update midcaps.py

--- midcaps.py
+++ midcaps.py
@@ -3,7 +3,7 @@ import pandas as pd
 def load_midcap_list():
   df = pd.read_csv('ind_niftymidcap150list.csv')
   df = pd.read_csv(path)
-    df["Symbol"] = df["Symbol"].astype(str) + ".NS"  # Append NSE suffix for yfinance
+  df["Symbol"] = df["Symbol"].astype(str) + ".NS"  # Append NSE suffix for yfinance
     return dict(zip(df["Company Name"], df["Symbol"]))
 
 midcap_stocks = load_midcap_list()
